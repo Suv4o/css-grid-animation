bc4558795dce7b976a7c7a5f7f664347e6f8d807
Aleks <aleksandar.trpkovski@gmail.com>
2023-04-08 12:42:48 +1000

    added load button

--- app.vue
+++ app.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
 import articles from "./data/articles";
+
+const pageSize = ref(12);
+const pageNumber = ref(1);
+const loadedArticles = ref<Object[]>([]);
+
+function paginate(array: Object[], pageSize: number, pageNumber: number) {
+    return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+}
+
+const loadArticles = computed(() => {
+    const articlesToLoad = paginate(articles, pageSize.value, pageNumber.value);
+    loadedArticles.value = [...loadedArticles.value, ...articlesToLoad];
+    return loadedArticles.value;
+});
 </script>
 
 <template>
-    <div class="bg-white">
+    <div>
         <div class="mx-auto container py-16 sm:py-24 px-6 lg:px-8">
             <AnimationGrid
-                :gridItems="articles"
-                :options="{ animateFrom: 'top', mode: 'random' }"
+                :gridItems="loadArticles"
+                :options="{ animateFrom: 'top', numberOfElements: 12 }"
                 class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
             >
                 <template #animation-grid-item="{ image, title }">
@@ -23,6 +37,15 @@ import articles from "./data/articles";
                     <h3 class="mt-4 text-2xl font-medium text-gray-700">{{ title }}</h3>
                 </template>
             </AnimationGrid>
+            <div class="mt-8 flex justify-center" v-if="loadArticles.length !== articles.length">
+                <button
+                    @click="pageNumber++"
+                    type="button"
+                    class="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500"
+                >
+                    LOAD MORE ARTICLES
+                </button>
+            </div>
         </div>
     </div>
 </template>
